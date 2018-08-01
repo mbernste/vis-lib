be5ba39fe05bb700f2ca60fdaa3049f30c799173
Matthew Bernstein <berniematt18@gmail.com>
2018-08-01 11:13:44 -0500

    Added colors

--- vis_lib.py
+++ vis_lib.py
@@ -8,12 +8,129 @@ import pandas
 import numpy as np
 from collections import defaultdict
 
+RED = '#ff0000'
+BLUE = '#0000ff'
+GREEN = '#00ff00'
+ORANGE = '#ed741d'
+PINK = '#ff00ff'
+WHITE = '#ffffff'
+
+NICE_RED = '#ff3633'
+NICE_BLUE = '#0066ff'
+NICE_GREEN = '#3dcd26'
+NICE_ORANGE = '#ee8420'
+NICE_PINK = '#d64edd'
+NICE_PURPLE = '#8200fc'
+
+MAROON = '#800000'
+SHRILL_BLUE = '#33d6ff'
+DARK_GREEN = '#004d00'
+ROTTING_ORANGE = '#b36b00'
+DARK_PINK = '#99004d'
+
+STANDARD_COLORS = [
+    RED,
+    BLUE,
+    GREEN,
+    ORANGE,
+    PINK
+]
+NICE_COLORS = [
+    NICE_RED, 
+    NICE_BLUE, 
+    NICE_GREEN, 
+    NICE_ORANGE, 
+    NICE_PINK
+]
+AGAINST_NICE_COLORS = [
+    MAROON,
+    SHRILL_BLUE,
+    DARK_GREEN,
+    ROTTING_ORANGE,
+    DARK_PINK
+]
+
+MANY_COLORS = [
+    "#0652ff", #  electric blue             
+    "#e50000", #  red                       
+    "#9a0eea", #  violet                    
+    "#01b44c", #  shamrock       
+    "#fedf08", #  dandelion                 
+    "#00ffff", #  cyan                      
+    "#89fe05", #  lime green                
+    "#a2cffe", #  baby blue                 
+    "#dbb40c", #  gold                      
+    "#029386", #  teal                      
+    "#ff9408", #  tangerine                 
+    "#d8dcd6", #  light grey                
+    "#80f9ad", #  seafoam                   
+    "#3d1c02", #  chocolate                 
+    "#fffd74", #  butter yellow             
+    "#536267", #  gunmetal
+    "#f6cefc", #  very light purple
+    "#650021", #  maroon
+    "#020035", #  midnight blue
+    "#b0dd16", #  yellowish green
+    "#9d7651", #  mocha
+    "#c20078", #  magenta
+    "#380282", #  indigo
+    "#ff796c", #  salmon
+    "#874c62", #  dark muave
+    "#02ccfe", #  bright sky blue
+    "#5fa052", #  muted green
+    "#9a3001", #  auburn
+    "#fc2647", #  pinky red
+    "#d8863b", #  dull orange
+    "#7b002c", #  bordeaux
+    "#8e82fe", #  periwinkle
+    "#ffff14", #  yellow 
+    "#ff073a", #  neon red
+    "#6ecb3c", #  apple
+    "#c45508", #  rust orange
+    "#8756e4", #  purpley
+    "#8756e4", #  diarrhea
+    "#bcecac", #  light sage
+    "#5d1451", #  grape purple
+    "#028f1e", #  emerald green
+    "#ffa62b", #  mango
+    "#3a2efe", #  light royal blue
+    "#c0022f", #  lipstick red
+    "#0485d1", #  cerulean
+    "#a57e52", #  puce
+    "#380835", #  eggplant
+    "#a9f971", #  spring green
+    "#fe4b03", #  blood orange
+    "#8cff9e", #  baby green
+    "#86775f", #  brownish grey
+    "#9d0759", #  dark fuchsia
+    "#665fd1", #  dark periwinkle
+    "#49759c", #  dullblue
+    "#fffa86", #  manilla
+    "#280137", #  midnight purple
+    "#fa4224", #  orangey red
+    "#d99b82", #  pinkish tan
+    "#152eff", #  vivid blue
+    "#f2ab15", #  squash
+    "#70b23f", #  nasty green
+    "#952e8f", #  warm purple
+    "#bcf5a6", #  washed out green
+    "#9a6200", #  raw sienna
+    "#fb5ffc", #  violet pink
+    "#ddd618", #  piss yellow
+    "#fe420f", #  orangered
+    "#c27e79", #  brownish pink
+    "#adf802", #  lemon green
+    "#29465b", #  dark grey blue
+    "#48c072", #  dark mint
+    "#edc8ff"  #  light lilac
+]
+
 def black_white_barplot(
-    data_frame, 
-    xcol, 
-    ycol, 
-    decimal=False, 
-    ax=None,
+        data_frame, 
+        xcol, 
+        ycol, 
+        decimal=False, 
+        ax=None,
     ):
     """
     Plot a black and white bar plot. 
@@ -68,9 +185,9 @@ def black_white_barplot(
                 fontsize=13
             )
 
-    sns.plt.ylabel(ycol, fontsize=15)
-    sns.plt.xlabel(xcol, fontsize=15)
-    sns.plt.tick_params(labelsize=13);
+    plt.ylabel(ycol, fontsize=15)
+    plt.xlabel(xcol, fontsize=15)
+    plt.tick_params(labelsize=13);
 
     return ax
 
@@ -130,9 +247,9 @@ def black_white_double_barplot(
                 fontsize=13
             )
 
-    sns.plt.ylabel(ycol, fontsize=15)
-    sns.plt.xlabel(xcol, fontsize=15)
-    sns.plt.tick_params(labelsize=13);
+    plt.ylabel(ycol, fontsize=15)
+    plt.xlabel(xcol, fontsize=15)
+    plt.tick_params(labelsize=13);
 
     return ax
 
@@ -249,8 +366,15 @@ def mult_var_strip_plot(
         y_label_font_size=15,
         x_tick_rotation=None
     ):
+    """
+    Stripplots in which each strip contains two points
+    comparing two methods. This figure is used to compare
+    the a metric across many labels between two partitions
+    of the data.
+    """
 
-    yticks = np.arange(0, data_frame.shape[0], 1)
+    print "Dataframe shape: %d" % data_frame.shape[0]
+    yticks = np.arange(0, data_frame.shape[0]/2, 1)
     ax.set_yticks(yticks)
 
     if x_lims:
@@ -301,8 +425,91 @@ def mult_var_strip_plot(
     else:
         ax.set_ylabel("")
 
-    return ax
 
+def performance_comparison_stripplot(
+        condition_to_label_to_value,
+        condition_var_name,
+        label_var_name,
+        value_var_name,
+        ax
+    ):
+
+    print "CONDITION_TO_LABEL_TO_VALUE"
+    print condition_to_label_to_value
+    print
+
+    conditions = list(
+        condition_to_label_to_value.keys()
+    )
+    assert len(conditions) == 2
+    c_1 = conditions[0]
+    c_2 = conditions[1]
+
+    da = [
+        (
+            label,
+            condition_to_label_to_value[c_1][label],
+            c_1
+        )
+        for label in condition_to_label_to_value[c_1]
+    ]
+
+    # Sort the data by the difference in value between 
+    # the first and second condition. This needs to be 
+    # done here because it seems Seaborn will order the 
+    # label stripplots in the order in which it finds 
+    # them in this list
+    da = sorted(
+        da, 
+        key=lambda x: abs(
+            x[1] - condition_to_label_to_value[c_2][x[0]]
+        ), 
+        reverse=True
+    )
+    labels_order = [
+        #x[0]
+        ""
+        for x 
+        in da
+    ]
+
+    da += [
+        (
+            label,
+            condition_to_label_to_value[c_2][label],
+            c_2
+        )
+        for label in condition_to_label_to_value[c_2]
+    ]
+
+    print "DA:\n%s" % da
+
+    df = pandas.DataFrame(
+        data=da,
+        columns=[
+            label_var_name, 
+            value_var_name,
+            condition_var_name
+        ]
+    )
+
+    print "DF:\n%s" % df
+
+    mult_var_strip_plot(
+        df,
+        value_var_name,
+        label_var_name,
+        condition_var_name,
+        ax=ax
+    )
+    
+    #ticks = ax.get_yticks().tolist()
+    #for tick_i in range(len(ticks)):
+    #    ticks[tick_i] = labels_order[tick_i]
+    #ax.set_yticklabels(ticks)
+    #ax.set_yticklabels(labels_order)
+
+    return labels_order
 
 def horizontal_bar_graph_clusters(
         label_to_df, 
@@ -354,7 +561,7 @@ def horizontal_bar_graph_clusters(
 
         ax.legend_.remove()
 
-    sns.plt.tight_layout() 
+    plt.tight_layout() 
     return fig, axarr
 
 
@@ -518,6 +725,8 @@ def grid_performance_comparison_scatterplots(
 
     for r_i, row in enumerate(variable_grid):
         for c_i, variable in enumerate(row):
+            if variable is None:
+                continue
             ax = axarr[r_i][c_i]
             df = variable_to_df[variable]
             if x_lim_grid and y_lim_grid:
